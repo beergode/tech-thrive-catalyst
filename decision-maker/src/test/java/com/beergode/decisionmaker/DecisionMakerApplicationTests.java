1e28fb94be1beba2ee4f76c66a0f01c0a9754498
Berkay Erten <berkayerten@BerkayEPTAG3498.fritz.box>
2024-03-15 21:44:44 +0100

    BG-NO_ISSUE-add-decision-making-module

---
+++ decision-maker/src/test/java/com/beergode/decisionmaker/DecisionMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beergode.decisionmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
