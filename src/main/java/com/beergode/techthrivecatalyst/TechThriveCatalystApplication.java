3480ed2ef3f3dab5c7d70fc6f7f56c912cd6e159
Berkay Erten <berkayerten@BerkayEPTAG3498.fritz.box>
2024-03-15 21:40:11 +0100

    BG-NO_ISSUE-init

---
+++ src/main/java/com/beergode/techthrivecatalyst/TechThriveCatalystApplication.java
@@ -0,0 +1,13 @@
+package com.beergode.techthrivecatalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechThriveCatalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechThriveCatalystApplication.class, args);
+	}
+
+}
