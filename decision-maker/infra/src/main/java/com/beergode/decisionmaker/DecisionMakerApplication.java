d4fa185041f721b8d561b4f6208f69360ffa22bc
Berkay Erten <berkayerten@BerkayEPTAG3498.fritz.box>
2024-03-15 22:16:35 +0100

    BG-introduce-domain-infra-setup WIP

--- decision-maker/infra/src/main/java/com/beergode/decisionmaker/DecisionMakerApplication.java
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/DecisionMakerApplication.java
@@ -1,13 +1,13 @@
-package com.beergode.techthrivecatalyst;
+package com.beergode.decisionmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechThriveCatalystApplication {
+public class DecisionMakerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechThriveCatalystApplication.class, args);
+		SpringApplication.run(DecisionMakerApplication.class, args);
 	}
 
 }
--- decision-maker/infra/src/test/java/com/beergode/decisionmaker/DecisionMakerApplicationTests.java
+++ decision-maker/infra/src/test/java/com/beergode/decisionmaker/DecisionMakerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.beergode.techthrivecatalyst;
+package com.beergode.decisionmaker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TechThriveCatalystApplicationTests {
+class DecisionMakerApplicationTests {
 
 	@Test
 	void contextLoads() {
