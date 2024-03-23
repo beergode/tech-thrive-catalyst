24c021a6bf29904364cb88b63b7d8cea1ef1d94c
mehmet.akpinar <mehmet.akpinar@xinerji.com>
2024-03-23 16:36:33 +0300

    Method added for test request

---
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/test/TestController.java
@@ -0,0 +1,18 @@
+package com.beergode.decisionmaker.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("BASARILI!");
+    }
+
+}
