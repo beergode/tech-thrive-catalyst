76a52303a612e2dffa6255011e9e08a7bd84e998
Berkay Erten <berkayertenn@gmail.com>
2024-03-24 15:54:36 +0100

    BG-update Cors for the endpoint

--- decision-maker/infra/src/main/java/com/beergode/decisionmaker/test/TestController.java
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/test/TestController.java
@@ -1,12 +1,14 @@
 package com.beergode.decisionmaker.test;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/decision-maker/test")
+@CrossOrigin
 public class TestController {
 
 
