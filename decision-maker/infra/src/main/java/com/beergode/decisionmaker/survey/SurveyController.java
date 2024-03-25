b10ced3ce7df9309c8cc2a74b6831687b40895f1
Berkay Erten <berkayertenn@gmail.com>
2024-03-25 19:05:58 +0100

    Query param addd

---
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/survey/SurveyController.java
@@ -0,0 +1,31 @@
+package com.beergode.decisionmaker.survey;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/decision-maker/surveys")
+@CrossOrigin
+public class SurveyController {
+
+    private static final Map<String, Survey> SURVEY_IN_DB = new HashMap<>();
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createSurvey(Survey createSurveyPayload) {
+        SURVEY_IN_DB.put(createSurveyPayload.id, createSurveyPayload);
+    }
+
+    @GetMapping("{id}")
+    public Survey createSurvey(@PathVariable String id) {
+        return SURVEY_IN_DB.get(id);
+    }
+
+    private static class Survey {
+        public String id;
+        public String content;
+    }
+}
