52dc5747b980c06a6631c996afc7be3afd6fd53b
mehmet.akpinar <mehmet.akpinar@xinerji.com>
2024-03-25 23:48:05 +0300

    DCM-2 DomainComponent added

--- decision-maker/infra/src/main/java/com/beergode/decisionmaker/DecisionMakerApplication.java
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/DecisionMakerApplication.java
@@ -1,9 +1,17 @@
 package com.beergode.decisionmaker;
 
+import com.beergode.decisionmaker.common.DomainComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		includeFilters = {
+				@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {DomainComponent.class})
+		}
+)
 public class DecisionMakerApplication {
 
 	public static void main(String[] args) {
