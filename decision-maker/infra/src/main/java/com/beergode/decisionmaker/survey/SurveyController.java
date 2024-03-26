455b780c8ddb530340f71b1927ddf1be3e2a0dc4
berkayerten <berkay.erten@haiilo.com>
2024-03-26 21:35:11 +0100

    DCM-dummy listing endpoint

--- decision-maker/infra/src/main/java/com/beergode/decisionmaker/survey/SurveyController.java
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/survey/SurveyController.java
@@ -1,12 +1,17 @@
 package com.beergode.decisionmaker.survey;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/decision-maker/surveys")
@@ -17,13 +22,19 @@ public class SurveyController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createSurvey(Survey createSurveyPayload) {
+    public Survey createSurvey(@RequestBody Survey createSurveyPayload) {
         SURVEY_IN_DB.put(createSurveyPayload.id, createSurveyPayload);
+        return createSurveyPayload;
     }
 
     @GetMapping("{id}")
-    public Survey createSurvey(@PathVariable String id) {
+    public Survey getSurvey(@PathVariable("id") String id) {
         return SURVEY_IN_DB.get(id);
+    }
+
+    @GetMapping
+    public List<Survey> listSurveys() {
+        return List.copyOf(SURVEY_IN_DB.values());
     }
 
     private static class Survey {
