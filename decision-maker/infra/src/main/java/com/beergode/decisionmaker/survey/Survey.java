24a09a9b8878ec4e93a402db3a2e0b8c3e55fed0
Berkay Erten <berkayertenn@gmail.com>
2024-03-26 20:48:46 +0100

    In mem temp controller

---
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/survey/Survey.java
@@ -0,0 +1,9 @@
+package com.beergode.decisionmaker.survey;
+
+import lombok.Data;
+
+@Data
+public class Survey {
+    private String id;
+    private String content;
+}
--- decision-maker/infra/src/main/java/com/beergode/decisionmaker/survey/SurveyController.java
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/survey/SurveyController.java
@@ -1,5 +1,7 @@
 package com.beergode.decisionmaker.survey;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
