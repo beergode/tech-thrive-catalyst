19803924d5787cb51946784be9c5dd0210798f39
berkayerten <berkay.erten@haiilo.com>
2024-04-10 01:21:41 +0200

    DCM trigger pipeline

--- decision-maker/infra/src/main/java/com/beergode/decisionmaker/DecisionMakerApplication.java
+++ decision-maker/infra/src/main/java/com/beergode/decisionmaker/DecisionMakerApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.FilterType;
 )
 public class DecisionMakerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DecisionMakerApplication.class, args);
 	}
