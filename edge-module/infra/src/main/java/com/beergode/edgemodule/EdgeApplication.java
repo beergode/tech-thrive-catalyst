869bbc5c97798f1e5cc7a41f847bad302dc02a3a
berkayerten <berkay.erten@haiilo.com>
2024-05-04 23:08:46 +0200

    DCM update infra of edge module

---
+++ edge-module/infra/src/main/java/com/beergode/edgemodule/EdgeApplication.java
@@ -0,0 +1,13 @@
+package com.beergode.edgemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeApplication.class, args);
+    }
+
+}
---
+++ edge-module/infra/src/test/java/com/beergode/edgemodule/EdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beergode.edgemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
